8529c20c43296b3e95882051f96d9cdd0d5727a2
Kamerr Ezz <kamerr_oficial@hotmail.com>
2023-12-23 19:33:14 -0600

    4.2

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import { db } from "./db.js";
 
+db.post.create({
+    data: {
+        user: {
+            create: {
+                name: 'luisita',
+                isActive: true
+            }
+        },
+        title: '50 cosas sobre mi parte 2',
+        content: "empezamos con las historias hacerca de las 50 cosas de mi.....",
+    }
+}).then(r => console.log(r))
