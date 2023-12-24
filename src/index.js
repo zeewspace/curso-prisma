974c4e91e2fbd68d6d281c5cbe10d116710e468d
Kamerr Ezz <kamerr_oficial@hotmail.com>
2023-12-23 19:02:20 -0600

    3.4

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import { db } from "./db.js";
 
-// db.users.findFirst({ where: { name: 'luisita' }}).then(r => console.log(r))
+db.users.findFirst({ where: { id: '5e185361-3a2c-4e27-8116-78babad983e0' }})
+.then(r => console.log(r))
 
-db.users.update({
-    data: {
-        age: 20
-    },
-    where: {
-        name: 'luisa'
-    }
-}).then(r => console.log(r))
+// db.users.delete({
+//     where: {
+//         id: '5e185361-3a2c-4e27-8116-78babad983e0'
+//     }
+// }).then(r => console.log(r))
